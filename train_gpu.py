7e1705bdc3dd6d0e63bf501d06ea74d98c628a5e
LiaoYK <lucky5877112@gmail.com>
2024-10-20 16:01:27 +0800

    add the gpu training version

--- train_gpu.py
+++ train_gpu.py
@@ -1,9 +1,10 @@
-import torch.cuda
+import time
+
+import torch
 import torchvision
+from torch import nn
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
-
-from model import *
 
 # prepare 數據集
 train_data = torchvision.datasets.CIFAR10("../data", True, torchvision.transforms.ToTensor(), download=True)
@@ -21,11 +22,42 @@ print("測試數據集的長度為：{}".format(test_data_size))
 train_dataloader = DataLoader(train_data, 64)
 test_dataloader = DataLoader(test_data, 64)
 
+# 設置執行設備
+# device = torch.device("cpu")
+device = torch.device("cuda:0")
+
+class Yukai(nn.Module):
+    def __init__(self):
+        super(Yukai, self).__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 32, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 32, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Flatten(),
+            nn.Linear(64 * 4 * 4, 64),
+            nn.Linear(64, 10)
+        )
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+
 # 創建網路模型
 yk = Yukai()
+# if torch.cuda.is_available():
+#     print("cuda OK!")
+# yk = yk.cuda()
+yk = yk.to(device)
 
 # 損失函數
 loss_fun = nn.CrossEntropyLoss()
+# if torch.cuda.is_available():
+#     loss_fun = loss_fun.cuda()
+loss_fun = loss_fun.to(device)
 
 # 優化器
 # learning rate = 0.001
@@ -44,10 +76,16 @@ writer = SummaryWriter("./logs_train")
 
 for i in range(epoch):
     print(f"---------第{i} 輪訓練開始---------")
+    start_time = time.time()
 
-    # 訓練步驟開始
+    # 訓練部驟開始
     for data in train_dataloader:
         imgs, targets = data
+        # if torch.cuda.is_available():
+        #     imgs = imgs.cuda()
+        #     targets = targets.cuda()
+        imgs = imgs.to(device)
+        targets = targets.to(device)
 
         outputs = yk(imgs)
         loss = loss_fun(outputs, targets)
@@ -62,21 +100,35 @@ for i in range(epoch):
         if total_train_step % 100 == 0:
             print(f"訓練次數：{total_train_step}，loss={loss}")
             writer.add_scalar("train_loss", loss.item(), total_train_step)
+            end_time = time.time()
+            calculation_time = end_time - start_time
+            print(f"這輪到目前為止花費了{calculation_time}s")
+
     # 測試步驟開始
     total_test_loss = 0
     total_accuracy = 0
     with torch.no_grad():
         for data in test_dataloader:
             imgs, targets = data
+
+            # if torch.cuda.is_available():
+            #     imgs = imgs.cuda()
+            #     targets = targets.cuda()
+            imgs = imgs.to(device)
+            targets = targets.to(device)
+
             outputs = yk(imgs)
             loss = loss_fun(outputs, targets)
             total_test_loss = total_test_loss + loss
             accurcy = (outputs.argmax(1) == targets).sum()
             total_accuracy = total_accuracy + accurcy
+
     print(f"整體測試集上的Loss: {total_test_loss}")
     print(f"整體測試集上的正確率: {total_accuracy / test_data_size}")
     writer.add_scalar("test_loss", total_test_loss, total_test_step)
     writer.add_scalar("test_accuracy", total_accuracy / test_data_size, total_test_step)
     total_test_step = total_test_step + 1
 
+    torch.save(yk, "yk_{}_.pth".format(i))
+    print("模型已保存！")
 writer.close()
